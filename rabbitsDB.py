b43bd2d4d313852b4bed997da1f391c2836c3f4f
Michael Cochran <mike.c.coder@gmail.com>
2024-05-03 13:19:19 -0500

    Add documents

--- rabbitsDB.py
+++ rabbitsDB.py
@@ -1,23 +1,25 @@
 import sqlite3
 import sys
 from os import system, name
+from datetime import datetime, timedelta
 
 con = sqlite3.connect("rabbits.db")
 cur = con.cursor()
 
-# This function takes the input to determine male or female and calls the corresponding function
-def gender():
-    gender_status = ""
-    while gender_status != "m" or gender_status != "M" or gender_status != "f" or gender_status != "F":
-        gender_status = input("Is this rabbit male or female? (m/f) ")
-        if gender_status == "m" or gender_status == "M":
-            gather_data_male()
-            break
-        elif gender_status == "f" or gender_status == "F":
-            gather_data_female()
-            break
-        else:
-            print("Please enter m or f ")
+# See what system is running to use appropriate command to clear the screen
+def clear():
+    if name == "nt":
+        system("cls")
+    else:
+        system("clear")
+
+def later_date(input_date, days):
+    # Convert input_date string to a datetime object
+    input_date = datetime.strptime(input_date, "%B %d")
+    # Add the specified number of days to the input_date
+    later_date = input_date + timedelta(days=days)
+    # Return the later date as a string
+    return later_date.strftime("%B %d")
 
 # Outputs the information stored in the database
 def view():
@@ -25,73 +27,41 @@ def view():
     while view != "y" or view != "Y" or view != "n" or view != "N":
         view = input("Would you like to see the database? ")
         if view == "y" or view == "Y":
-            print("Male")
-            # Print Male table
-            for row in cur.execute("SELECT * FROM male_rabbits"):
-                print(row)
             print("Female")
-            # Print Female table
+            # Print data in the table
             for row in cur.execute("SELECT * FROM female_rabbits"):
-                print(row)
-            break
-
+                print("\t".join(str(cell) for cell in row))
+            # break
         elif view == "n" or view == "N":
             # Close the connection when done with it
             con.close()
             sys.exit()
 
 def create_tables(cur):
-    # Create table for the males with certain criterion
-    cur.execute('''CREATE TABLE IF NOT EXISTS male_rabbits
-                (name text, last_bred text, doe text, kits_born text, eight_weeks_weight text, comments text)''')
-
     # Create table for the females with certain criterion
     cur.execute('''CREATE TABLE IF NOT EXISTS female_rabbits
-                (name text, last_bred text, buck text, nest_date text, due_date text, kindled tex, kits_born_alive text,
-                kits_born_dead text, kits_weaned text, eight_weeks_weight text, comments text)''')
-
-def gather_data_male():
-    name = input("What is the name of the rabbit? ")
-    bred = input("What date was it bred? ")
-    doe = input("Which doe was bred? ")
-    kits = input("How many kits resulted from this breeding? ")
-    weight = input("What is the average weight of the kits at 8 weeks? ")
-    comments = input("What other comments do you have? ")
-    insert_data_male(name, bred, doe, kits, weight, comments)
+                (name text, last_bred text, buck text, palpatate text, nest_date text, due_date text, comments text)''')
 
 def gather_data_female():
+    print("Please enter the following information for the doe.")
     name = input("What is the name of the rabbit? ")
-    bred = input("What date was it bred? ")
+    bred = input("What date was it bred? (Month and day, ex. May 5) ")
     buck = input("Which buck was bred? ")
-    nest = input("What is the date for the nest box? (28 days after breeding) ")
-    due = input("What is the projected due date? (31 days after breeding) ")
-    kindled = input("What date did she kindle? ")
-    kits = input("How many live kits resulted from this breeding? ")
-    dead = input("How many dead kits were there? ")
-    weaned = input("How many kits survived past weaning? ")
-    weight = input("What is the average weight of the kits at 8 weeks? ")
+    # weight = input("What is the average weight of the kits at 8 weeks? ")
     comments = input("What other comments do you have? ")
-    insert_data_female(name, bred, buck, nest, due, kindled, kits, dead, weaned, weight, comments)
+    palpatating = later_date(bred, 15)
+    nest = later_date(bred, 27)
+    due = later_date(bred, 31)
+    print(palpatating, nest, due)
+    insert_data_female(name, bred, buck, palpatating, nest, due, comments)
+    # gather_data_male()
 
-def insert_data_male(name, bred, doe, kits, weight, comments):
+def insert_data_female(name, bred, buck, palpatating, nest, due, comments):
     # Insert a row of data
-    cur.execute("INSERT INTO male_rabbits VALUES (?, ?, ?, ?, ?, ?)",(name, bred, doe, kits, weight, comments))
-
-    # Save (commit) the changes to the database
-    con.commit()
-    print("Male added to file")
-
-    # View database prompt
-    view()
-
-def insert_data_female(name, bred, buck, nest, due, kindled, kits, dead, weaned, weight, comments):
-    # Insert a row of data
-    cur.execute("INSERT INTO female_rabbits VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(name, bred, buck, nest, due, kindled, kits, dead, weaned, weight, comments))
-
+    cur.execute("INSERT INTO female_rabbits VALUES (?, ?, ?, ?, ?, ?, ?)",(name, bred, buck, palpatating, nest, due, comments))
     # Save (commit) the changes to the database
     con.commit()
     print("Female added to file")
-
     # View database prompt
     view()
 
@@ -100,29 +70,27 @@ def insert_data_female(name, bred, buck, nest, due, kindled, kits, dead, weaned,
 
 if __name__ == "__main__":
     create_tables(cur)
-
-    # See what system is running and use appropriate command to clear the screen
-    if name == "nt":
-        system("cls")
-    else:
-        system("clear")
+    
+    clear()
 
     # Print blank line before anything happens and then look for input for view or input data and call appropriate function
     print("")
     input_or_view = ""
     while input_or_view != "i" or input_or_view != "I" or input_or_view != "v" or input_or_view != "V":
-        input_or_view = input("Would you like to Input data or View data? (I/V) ")
+        input_or_view = input("Would you like to Input data, View data, or Exit? (I/V/E) ")
         if input_or_view == "i" or input_or_view == "I":
             print("\n")
-            gender()
-            break
+            # gender()
+            gather_data_female()
         elif input_or_view == "v" or input_or_view == "V":
             print("\n")
-            print("Male")
-            for row in cur.execute("SELECT * FROM male_rabbits"):
-                print(row)
+            # print("Male")
+            # for row in cur.execute("SELECT * FROM male_rabbits"):
+            #     print(row)
             print("Female")
             for row in cur.execute("SELECT * FROM female_rabbits"):
                 print(row)
-            print("\n\n")
+            print("\n")
+        elif input_or_view == "e" or input_or_view == "E":
+            clear()
             break
