d735865df6eebe7794c7f91604b518dcb53afb8c
Michael Cochran <mike.c.coder@gmail.com>
2024-05-07 20:32:03 -0500

    DRY code for viewing

--- breeding.py
+++ breeding.py
@@ -23,50 +23,42 @@ def later_date(input_date, days):
 
 # Outputs the information stored in the database
 def view():
-    view = ""
-    while view != "y" or view != "Y" or view != "n" or view != "N":
-        view = input("Would you like to see the database? ")
-        if view == "y" or view == "Y":
-            print("Female")
+            print("Female, Breed Date, Buck, Palpatate, Nest Box, Kindling Date, Comments")
             # Print data in the table
-            for row in cur.execute("SELECT * FROM female_rabbits"):
+            for row in cur.execute("SELECT * FROM rabbits"):
                 print("\t".join(str(cell) for cell in row))
             # break
-        elif view == "n" or view == "N":
-            # Close the connection when done with it
-            con.close()
-            sys.exit()
+            print("\n")
+            return
+            
 
 def create_tables(cur):
     # Create table for the females with certain criterion
-    cur.execute('''CREATE TABLE IF NOT EXISTS female_rabbits
+    cur.execute('''CREATE TABLE IF NOT EXISTS rabbits
                 (name text, last_bred text, buck text, palpatate text, nest_date text, due_date text, comments text)''')
 
-def gather_data_female():
+def gather_data():
     print("Please enter the following information for the doe.")
     name = input("What is the name of the rabbit? ")
     bred = input("What date was it bred? (Month and day, ex. May 5) ")
     buck = input("Which buck was bred? ")
-    # weight = input("What is the average weight of the kits at 8 weeks? ")
     comments = input("What other comments do you have? ")
     palpatating = later_date(bred, 15)
     nest = later_date(bred, 27)
     due = later_date(bred, 31)
     print(palpatating, nest, due)
-    insert_data_female(name, bred, buck, palpatating, nest, due, comments)
-    # gather_data_male()
+    insert_data(name, bred, buck, palpatating, nest, due, comments)
 
-def insert_data_female(name, bred, buck, palpatating, nest, due, comments):
+def insert_data(name, bred, buck, palpatating, nest, due, comments):
     # Insert a row of data
-    cur.execute("INSERT INTO female_rabbits VALUES (?, ?, ?, ?, ?, ?, ?)",(name, bred, buck, palpatating, nest, due, comments))
+    cur.execute("INSERT INTO rabbits VALUES (?, ?, ?, ?, ?, ?, ?)",(name, bred, buck, palpatating, nest, due, comments))
     # Save (commit) the changes to the database
     con.commit()
-    print("Female added to file")
+    print("Data added to file")
     # View database prompt
     view()
 
-# cur.execute("DROP TABLE female_rabbits")
-# cur.execute("DROP TABLE male_rabbits")
+# cur.execute("DROP TABLE rabbits")
 
 if __name__ == "__main__":
     create_tables(cur)
@@ -80,17 +72,13 @@ if __name__ == "__main__":
         input_or_view = input("Would you like to Input data, View data, or Exit? (I/V/E) ")
         if input_or_view == "i" or input_or_view == "I":
             print("\n")
-            # gender()
-            gather_data_female()
+            gather_data()
         elif input_or_view == "v" or input_or_view == "V":
-            print("\n")
-            # print("Male")
-            # for row in cur.execute("SELECT * FROM male_rabbits"):
-            #     print(row)
-            print("Female")
-            for row in cur.execute("SELECT * FROM female_rabbits"):
-                print(row)
-            print("\n")
+            clear()
+            view()
         elif input_or_view == "e" or input_or_view == "E":
             clear()
             break
+
+
+            con.close()
