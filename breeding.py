1a4eba3ee214a484c766b5c4ec3318b873e0f2e1
Michael Cochran <mike.c.coder@gmail.com>
2024-05-07 21:52:38 -0500

    Refactor code

--- breeding.py
+++ breeding.py
@@ -1,17 +1,15 @@
 import sqlite3
 import sys
-from os import system, name
+import os
 from datetime import datetime, timedelta
+import time
 
 con = sqlite3.connect("rabbits.db")
 cur = con.cursor()
 
 # See what system is running to use appropriate command to clear the screen
 def clear():
-    if name == "nt":
-        system("cls")
-    else:
-        system("clear")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def later_date(input_date, days):
     # Convert input_date string to a datetime object
@@ -23,42 +21,71 @@ def later_date(input_date, days):
 
 # Outputs the information stored in the database
 def view():
-            print("Female, Breed Date, Buck, Palpatate, Nest Box, Kindling Date, Comments")
+            print("Female\tBreed Date\tBuck\tPalpate\tNest Box\tKindling Date\tComments")
             # Print data in the table
-            for row in cur.execute("SELECT * FROM rabbits"):
-                print("\t".join(str(cell) for cell in row))
-            # break
-            print("\n")
+            rows = cur.fetchall()
+            for row in rows:
+                print("\t".join(map(str, row)))
             return
-            
 
 def create_tables(cur):
     # Create table for the females with certain criterion
-    cur.execute('''CREATE TABLE IF NOT EXISTS rabbits
-                (name text, last_bred text, buck text, palpatate text, nest_date text, due_date text, comments text)''')
-
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS rabbits (
+            name TEXT,
+            last_bred TEXT,
+            buck TEXT,
+            palpatate TEXT,
+            nest_date TEXT,
+            due_date TEXT,
+            comments TEXT
+        )
+    ''')
+    
 def gather_data():
-    print("Please enter the following information for the doe.")
-    name = input("What is the name of the rabbit? ")
-    bred = input("What date was it bred? (Month and day, ex. May 5) ")
-    buck = input("Which buck was bred? ")
-    comments = input("What other comments do you have? ")
-    palpatating = later_date(bred, 15)
-    nest = later_date(bred, 27)
-    due = later_date(bred, 31)
+    clear()
+    print("Please enter the following information.")
+
+    # Define questions and corresponding keys
+    questions = {
+        "name": "What is the name of the doe? ",
+        "bred": "What date was she bred? (Month and day, ex. May 5) ",
+        "buck": "Which buck was bred? ",
+        "comments": "What other comments do you have? "
+    }
+
+    # Gather data
+    data = {}
+    for key, question in questions.items():
+        data[key] = input(question)
+
+    # Calculate dates
+    palpatating, nest, due = later_date(data["bred"], 15), later_date(data["bred"], 27), later_date(data["bred"], 31)
+
+    # Display dates
+    print("\nPalpatate, Nest box, Due")
     print(palpatating, nest, due)
-    insert_data(name, bred, buck, palpatating, nest, due, comments)
 
-def insert_data(name, bred, buck, palpatating, nest, due, comments):
-    # Insert a row of data
-    cur.execute("INSERT INTO rabbits VALUES (?, ?, ?, ?, ?, ?, ?)",(name, bred, buck, palpatating, nest, due, comments))
-    # Save (commit) the changes to the database
-    con.commit()
-    print("Data added to file")
-    # View database prompt
-    view()
+    # Insert data
+    insert_data(data["name"], data["bred"], data["buck"], palpatating, nest, due, data["comments"])
 
-# cur.execute("DROP TABLE rabbits")
+def insert_data(name, bred, buck, palpitating, nest, due, comments):
+    try:
+        # Insert a row of data
+        cur.execute("INSERT INTO rabbits VALUES (?, ?, ?, ?, ?, ?, ?)", (name, bred, buck, palpitating, nest, due, comments))
+        # Save (commit) the changes to the database
+        con.commit()
+        print("\nData added to file")
+    except Exception as e:
+        # Handle exceptions gracefully
+        print("Error:", e)
+    finally:
+        # Return to the main options after a brief delay
+        time.sleep(2)
+        clear()
+
+    return
+
 
 if __name__ == "__main__":
     create_tables(cur)
@@ -67,18 +94,17 @@ if __name__ == "__main__":
 
     # Print blank line before anything happens and then look for input for view or input data and call appropriate function
     print("")
-    input_or_view = ""
-    while input_or_view != "i" or input_or_view != "I" or input_or_view != "v" or input_or_view != "V":
-        input_or_view = input("Would you like to Input data, View data, or Exit? (I/V/E) ")
-        if input_or_view == "i" or input_or_view == "I":
+    while True:
+        input_or_view = input("Would you like to Input data, View data, or Exit? (I/V/E) ").lower()
+        if input_or_view == "i":
             print("\n")
             gather_data()
-        elif input_or_view == "v" or input_or_view == "V":
+        elif input_or_view == "v":
             clear()
             view()
-        elif input_or_view == "e" or input_or_view == "E":
+        elif input_or_view == "e":
             clear()
-            break
-
-
             con.close()
+            sys.exit()
+        else:
+            print("Invalid input. Please enter 'I' to input data, 'V' to view data, or 'E' to exit.")
