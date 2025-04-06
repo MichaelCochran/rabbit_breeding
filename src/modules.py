b1a968b51592fb40a168f7bb63d7ce95dcff8da6
Michael Cochran <mike.c.coder@gmail.com>
2025-04-05 20:59:47 -0500

    Remove duplicate lines from `modules.py`

--- src/modules.py
+++ src/modules.py
@@ -2,6 +2,4 @@ import sys
 from datetime import datetime, timedelta
 from database_helper import DatabaseHelper
 from utils import Utils
-from database_helper import DatabaseHelper
-from utils import Utils
 db = DatabaseHelper("rabbits.db")
